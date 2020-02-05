2f11fedd261a9db7f65f5bbf0f029819da76947e
Billy He <hello@billyhe.com>
2020-02-04 21:06:45 -0800

    Update restore card middleware to handle deferred app ready event

--- src/middleware/card/restore-card-detail.middleware.js
+++ src/middleware/card/restore-card-detail.middleware.js
@@ -1,22 +1,38 @@
 import Promise from "bluebird";
+import flow from "lodash/fp/flow";
+import map from "lodash/fp/map";
 
+import { READY } from "/src/action/app.action";
 import { ADD, restore } from "/src/action/card.action";
 import { getCardDetailService } from "/src/services/card-detail.service";
 
-export default ({ getState, dispatch }) => next => action => {
-  const { type: actionType } = action;
+export default ({ getState, dispatch }) => {
+  let appReady = false;
+  let pendingCardIds = [];
 
-  return Promise.resolve(next(action)).then(() => {
-    if (actionType === ADD) {
-      const {
-        payload: { id: cardId }
-      } = action;
+  const restoreCardById = cardId =>
+    getCardDetailService()
+      .getDetailByCardId(getState, cardId)
+      .then(cardDetail => dispatch(restore({ id: cardId, ...cardDetail })));
 
-      return getCardDetailService()
-        .getDetailByCardId(getState, cardId)
-        .then(cardDetail => {
-          return dispatch(restore({ id: cardId, ...cardDetail }));
-        });
-    }
-  });
+  return next => action => {
+    const { type: actionType } = action;
+
+    return Promise.resolve(next(action)).then(() => {
+      if (actionType === READY) {
+        appReady = true;
+        return flow([map(restoreCardById), Promise.all])(pendingCardIds);
+      } else if (actionType === ADD) {
+        const {
+          payload: { id: cardId }
+        } = action;
+
+        if (appReady) {
+          return restoreCardById(cardId);
+        } else {
+          pendingCardIds = [...pendingCardIds, cardId];
+        }
+      }
+    });
+  };
 };
