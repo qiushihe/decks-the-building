b7d482579e5a7539786a68d72dbea5f16a3ba335
Billy He <hello@billyhe.com>
2020-02-08 00:23:37 -0800

    Add controls to add cards to stack form

--- src/component/modal/add-cards-to-stack/add-cards-to-stack.jsx
+++ src/component/modal/add-cards-to-stack/add-cards-to-stack.jsx
@@ -2,18 +2,45 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import Arrange from "/src/component/arrange";
 import BaseModal from "/src/component/modal/base";
 
-const Base = styled.div``;
+const Header = styled.div``;
+
+const StyledTextarea = styled.textarea`
+  width: 420px;
+  height: 120px;
+  white-space: nowrap;
+  resize: none;
+`;
 
 export class AddCardsToStack extends React.PureComponent {
   render() {
     const { hideModal } = this.props;
     return (
       <BaseModal>
-        <Base>
-          <button onClick={hideModal}>Close</button>
-        </Base>
+        <Arrange vertical={true}>
+          <Arrange.Fit>
+            <Header>Add Cards to Stack</Header>
+          </Arrange.Fit>
+          <Arrange.Fit>&nbsp;</Arrange.Fit>
+          <Arrange.Fit>
+            <StyledTextarea />
+          </Arrange.Fit>
+          <Arrange.Fit>&nbsp;</Arrange.Fit>
+          <Arrange.Fit>
+            <Arrange>
+              <Arrange.Fill>&nbsp;</Arrange.Fill>
+              <Arrange.Fit>
+                <button onClick={hideModal}>Close</button>
+              </Arrange.Fit>
+              <Arrange.Fit>&nbsp;&nbsp;</Arrange.Fit>
+              <Arrange.Fit>
+                <button onClick={() => {}}>Submit</button>
+              </Arrange.Fit>
+            </Arrange>
+          </Arrange.Fit>
+        </Arrange>
       </BaseModal>
     );
   }
