5df92e89eab27305abc911ec411fbf2c1c19da1e
Billy He <billy@metalabdesign.com>
2020-02-11 15:46:04 -0800

    Add random suffix ID generation

---
+++ aws/random-id.lambda.js
@@ -0,0 +1,52 @@
+// Use https://closure-compiler.appspot.com to compile/minify
+
+const https = require("https");
+const url = require("url");
+
+const newId = (len, radix) => {
+  const alphabet =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
+  const chars = alphabet.split("");
+
+  const newId = [];
+
+  radix = radix || chars.length;
+  len = len || 22;
+
+  let i = 0;
+  for (i = 0; i < len; i++) {
+    newId[i] = chars[0 | (Math.random() * radix)];
+  }
+
+  return newId.join("");
+};
+
+exports.handler = (evt, ctx) => {
+  const responseBody = JSON.stringify({
+    Status: "SUCCESS",
+    PhysicalResourceId: ctx.logStreamName,
+    StackId: evt.StackId,
+    RequestId: evt.RequestId,
+    LogicalResourceId: evt.LogicalResourceId,
+    Data: {
+      Value: newId(10, 62)
+    }
+  });
+
+  const parsedUrl = url.parse(evt.ResponseURL);
+
+  const request = https.request(
+    {
+      hostname: parsedUrl.hostname,
+      port: 443,
+      path: parsedUrl.path,
+      method: "PUT",
+      headers: { "content-length": responseBody.length }
+    },
+    () => ctx.done()
+  );
+
+  request.on("error", () => ctx.done());
+  request.write(responseBody);
+  request.end();
+};
