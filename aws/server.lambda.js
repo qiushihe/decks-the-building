270391333a3c0063398d6e83fe4ef30e6af71ba3
Billy He <billy@metalabdesign.com>
2020-02-10 21:10:08 -0800

    Add note about HTTP Basic auth

--- aws/server.lambda.js
+++ aws/server.lambda.js
@@ -1,5 +1,9 @@
 // Use https://closure-compiler.appspot.com to compile/minify
 
+// TODO: HTTP Basic:
+//       * https://gist.github.com/lmakarov/e5984ec16a76548ff2b278c06027f1a4
+//       * https://medium.com/hackernoon/serverless-password-protecting-a-static-website-in-an-aws-s3-bucket-bfaaa01b8666
+
 exports.handler = (evt, ctx) => {
   ctx.succeed("<h1>It CloudFormation!</h1>");
 };
