8e53a23960a9481f92256788c07da1857efe308b
Billy He <hello@billyhe.com>
2020-02-11 22:51:08 -0800

    Update conditional authorizer

--- aws/http-basic-authorizer.lambda.js
+++ aws/http-basic-authorizer.lambda.js
@@ -1,30 +1,10 @@
 // Use https://closure-compiler.appspot.com to compile/minify
 
-exports.handler = function(event, context, callback) {
-  const authorizationHeader =
-    event.headers["authorization"] || event.headers["Authorization"];
-
-  if (!authorizationHeader) {
-    return callback("Unauthorized");
-  }
-
-  const encodedCreds = authorizationHeader.split(" ")[1];
-  const plainCreds = new Buffer(encodedCreds, "base64").toString().split(":");
-  const username = plainCreds[0];
-  const password = plainCreds[1];
-
-  if (!(username === "admin" && password === "secret")) {
-    return callback("Unauthorized");
-  }
-
-  callback(null, buildAllowAllPolicy(event, username));
-};
-
-function buildAllowAllPolicy(event, principalId) {
-  const tmp = event.methodArn.split(":");
-  const apiGatewayArnTmp = tmp[5].split("/");
-  const awsAccountId = tmp[4];
-  const awsRegion = tmp[3];
+const buildPolicy = (event, principalId) => {
+  const methodArnParts = event.methodArn.split(":");
+  const apiGatewayArnTmp = methodArnParts[5].split("/");
+  const awsAccountId = methodArnParts[4];
+  const awsRegion = methodArnParts[3];
   const restApiId = apiGatewayArnTmp[0];
   const stage = apiGatewayArnTmp[1];
   const apiArn =
@@ -42,13 +22,31 @@ function buildAllowAllPolicy(event, principalId) {
     principalId: principalId,
     policyDocument: {
       Version: "2012-10-17",
-      Statement: [
-        {
-          Action: "execute-api:Invoke",
-          Effect: "Allow",
-          Resource: [apiArn]
-        }
-      ]
+      Statement: [{
+        Action: "execute-api:Invoke",
+        Effect: "Allow",
+        Resource: [apiArn]
+      }]
     }
   };
-}
+};
+
+exports.handler = function(event, context, callback) {
+  const authorizationHeader =
+    event.headers["authorization"] || event.headers["Authorization"];
+
+  if (!authorizationHeader) {
+    return callback("Unauthorized");
+  }
+
+  const encodedAuthorization = authorizationHeader.split(" ")[1];
+  const decidedAuthorization = new Buffer(encodedAuthorization, "base64").toString().split(":");
+  const username = decidedAuthorization[0];
+  const password = decidedAuthorization[1];
+
+  if (!(username === "admin" && password === "secret")) {
+    return callback("Unauthorized");
+  }
+
+  callback(null, buildPolicy(event, username));
+};
