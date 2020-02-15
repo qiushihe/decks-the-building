3e81be7c73ed602d2b439942451ed41ce813595c
Billy He <billy@metalabdesign.com>
2020-02-14 16:32:42 -0800

    Create default workspace when there is none

--- src/middleware/workspace/restore-workspaces.middleware.js
+++ src/middleware/workspace/restore-workspaces.middleware.js
@@ -1,13 +1,15 @@
 import Promise from "bluebird";
+import uuidV4 from "uuid/v4";
 import flow from "lodash/fp/flow";
 import map from "lodash/fp/map";
 import first from "lodash/fp/first";
 import cond from "lodash/fp/cond";
 import negate from "lodash/fp/negate";
 import isNil from "lodash/fp/isNil";
+import isEmpty from "lodash/fp/isEmpty";
 
 import { READY } from "/src/action/app.action";
-import { activate } from "/src/action/workspace.action";
+import { create, activate } from "/src/action/workspace.action";
 import { getFetchAllFromLocalService } from "/src/service/workspace/fetch-all-from-local.service";
 
 import importFromJson from "./import-from-json";
@@ -19,13 +21,25 @@ export default ({ dispatch }) => next => action => {
     if (actionType === READY) {
       return getFetchAllFromLocalService()
         .retrieveAll()
-        .then(flow([map(data => importFromJson(dispatch, data)), Promise.all]))
-        .then(
-          flow([
-            first,
-            cond([[negate(isNil), id => dispatch(activate({ id }))]])
-          ])
-        );
+        .then(workspacesData => {
+          if (isEmpty(workspacesData)) {
+            return dispatch(
+              create({ id: uuidV4(), label: "Untitled" })
+            ).then(({ payload: { id } }) => dispatch(activate({ id })));
+          } else {
+            const importedFromJson = flow([
+              map(data => importFromJson(dispatch, data)),
+              Promise.all
+            ])(workspacesData);
+
+            return importedFromJson.then(
+              flow([
+                first,
+                cond([[negate(isNil), id => dispatch(activate({ id }))]])
+              ])
+            );
+          }
+        });
     }
   });
 };
