c90a4fa2999c7523de6bc491115579b224a7756e
Billy He <hello@billyhe.com>
2020-03-10 23:12:36 -0700

    Remove unused icon imports

--- src/component/icon/index.jsx
+++ src/component/icon/index.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 
 import {
-  Edit,
-  AddCircleOutlineRounded,
-  PostAddRounded,
-  LayersOutlined,
-  SaveOutlined,
-  FilterDramaOutlined,
   FlipToFrontOutlined,
   FullscreenRounded,
   FullscreenExitRounded,
@@ -28,15 +22,7 @@ export const WorkspaceIcon = makeIconHoc(TableChartRounded);
 export const LaneIcon = makeIconHoc(ViewCarouselRounded);
 export const StackIcon = makeIconHoc(ViewListRounded);
 
-export const RenameIcon = makeIconHoc(Edit);
 export const DeleteIcon = makeIconHoc(RemoveCircleOutlineRounded);
-export const CreateIcon = makeIconHoc(AddCircleOutlineRounded);
-
-export const AddCardsIcon = makeIconHoc(PostAddRounded);
-export const CombineCardsIcon = makeIconHoc(LayersOutlined);
-
-export const SaveIcon = makeIconHoc(SaveOutlined);
-export const ImportExportIcon = makeIconHoc(FilterDramaOutlined);
 
 export const AddOneIcon = makeIconHoc(ExposurePlus1Rounded);
 export const SubtractOneIcon = makeIconHoc(ExposureNeg1Rounded);
